6626d8431299a2d0b76913660eae390c4f552ced
reinalde <rainalde18@gmail.com>
2025-03-08 18:46:59 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -4,23 +4,27 @@
 
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("meu-pwa-cache").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/slide.css",
-          "/slide.js",
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  // event.waitUntil(
+  //   caches.open("meu-pwa-cache").then((cache) => {
+  //     return cache.addAll([
+  //       "/",
+  //       "/index.html",
+  //       "/slide.css",
+  //       "/slide.js",
+  //     ]);
+  //   })
+  // );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const lastUrl = await event.clientId
+        ? (await self.clients.get(event.clientId)).postMessage({ type: "getUrl" })
+        : null;
+
+      const url = lastUrl || event.request.url;
+      return fetch(url);
+    })()
+  );
+});
