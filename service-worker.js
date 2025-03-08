292056b1f266de78447b1b87608f6364e8c5d2da
reinalde <rainalde18@gmail.com>
2025-03-08 17:10:01 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,26 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-workbox.recipes.googleFontsCache();
+// workbox.recipes.googleFontsCache();
+
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("meu-pwa-cache").then((cache) => {
+        return cache.addAll([
+          "/",
+          "/index.html",
+          "/slide.css",
+          "/slide.js",
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
